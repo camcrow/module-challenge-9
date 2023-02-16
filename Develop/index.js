2312c2132b730721dc2b8d1381f00efff5925c41
camcrow <CameronCroteau2020@gmail.com>
2023-02-15 20:47:07 -0600

    c

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,145 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require(/utils/generateMarkdown)
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+        name:'title',
+        type: 'input',
+        message: 'Enter title of Project',
+        validate: chosenTitle => {
+            if(chosenTitle) {
+                return true;
+            } else{
+                return false;
+            }
+            }
+        },
+        {
+            name:'description',
+            type: 'input',
+            message: 'Describe your project and what purpose it serves.',
+            validate: chosenDes => {
+                if(chosenDes) {
+                    return true;
+                } else{
+                    return false;
+                }
+                }
+            },
+            {
+                name:'table-of-contents',
+                type: 'input',
+                message: 'Enter the table of contents for your project.',
+                validate: chosenTable => {
+                    if(chosenTable) {
+                        return true;
+                    } else{
+                        return false;
+                    }
+                    }
+                },
+                {
+                    name:'installation',
+                    type: 'input',
+                    message: 'Give a guide on how to install this project.',
+                    validate: chosenTable => {
+                        if(chosenTable) {
+                            return true;
+                        } else{
+                            return false;
+                        }
+                        }
+                    },
+                    {
+                        name:'usage',
+                        type: 'input',
+                        message: 'What is the intended usage of this project?',
+                        validate: chosenTable => {
+                            if(chosenTable) {
+                                return true;
+                            } else{
+                                return false;
+                            }
+                            }
+                        },
+                        {
+                            name:'license',
+                            type: 'input',
+                            message: 'What license does your project use?',
+                            choices: ['apache','MIT License','GNU General Public License','none']
+                            },    
+                                             
+                        {
+                        name:'contributers',
+                        type: 'input',
+                        message: 'What are the contributer guidelines?',
+                        validate: chosenTable => {
+                            if(chosenTable) {
+                                return true;
+                            } else{
+                                return false;
+                            }
+                            }
+                        },
+                        {
+                            name:'test',
+                            type: 'input',
+                            message: 'Enter instructions on testing the application.',
+                            validate: chosenTable => {
+                                if(chosenTable) {
+                                    return true;
+                                } else{
+                                    return false;
+                                }
+                                }
+                            },
+                            {
+                                name:'githubUsername',
+                                type: 'input',
+                                message: 'Enter your GitHub Username',
+                                validate: chosenTable => {
+                                    if(chosenTable) {
+                                        return true;
+                                    } else{
+                                        return false;
+                                    }
+                                    }
+                                },
+                                {
+                                    name:'email',
+                                    type: 'input',
+                                    message: 'Enter your email',
+                                    validate: chosenTable => {
+                                        if(chosenTable) {
+                                            return true;
+                                        } else{
+                                            return false;
+                                        }
+                                        }
+                                    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let content = generateMarkdown(data);
+    fs.writeFile(fileName, content, function (error){
+        if (error) {
+            return console.log(error)
+        }
+        else {
+            console.log('success')}
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data){
+        const newFile = 'README.md';
+        writeToFile(newFile, data)
+    });
+}
 
 // Function call to initialize app
 init();
